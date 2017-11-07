f07e5781ffd72a004e4526cc2f74cb0fc5d00f29
Eduardo Aguilera <laloao.2302@gmail.com>
2017-11-07 10:57:31 -0600

    Initial Commit

---
+++ FURApp_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FURApp_iOS
+//
+//  Created by Eduardo Aguilera Olascoaga on 11/7/17.
+//  Copyright © 2017 Fundación Rescate Arboreo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FURApp_iOSTests/FURApp_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  FURApp_iOSTests.swift
+//  FURApp_iOSTests
+//
+//  Created by Eduardo Aguilera Olascoaga on 11/7/17.
+//  Copyright © 2017 Fundación Rescate Arboreo. All rights reserved.
+//
+
+import XCTest
+@testable import FURApp_iOS
+
+class FURApp_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
